c15fb8dd4d4e554f0dc64b060798681b828e2fef
Prithviraj383 <prithvichidri5@gmail.com>
2026-01-01 16:15:51 +0530

    5th program fix

--- DS/5.c
+++ DS/5.c
@@ -25,27 +25,27 @@ int main(int argc, char *argv[])
     int option;
     do
     {
-        printf(“\n\n * ****MAIN MENU * ****”);
-        printf(“\n 1 : Create a list”);
-        printf(“\n 2 : Display the list”);
-        printf(“\n 3 : Add a node at the beginning”);
-        printf(“\n 4 : Add a node at the end”);
-        printf(“\n 5 : Add a node before a given node”);
-        printf(“\n 6 : Add a node after a given node”);
-        printf(“\n 7 : Delete a node from the beginning”);
-        printf(“\n 8 : Delete a node from the end”);
-        printf(“\n 9 : Delete a given node”);
-        printf(“\n 10 : Delete a node after a given node”);
-        printf(“\n 11 : Delete the entire list”);
-        printf(“\n 12 : Sort the list”);
-        printf(“\n 13 : EXIT”);
-        printf(“\n\n Enter your option : “);
-        scanf(“% d”, &option);
+        printf("\n\n * ****MAIN MENU * ****");
+        printf("\n 1 : Create a list");
+        printf("\n 2 : Display the list");
+        printf("\n 3 : Add a node at the beginning");
+        printf("\n 4 : Add a node at the end");
+        printf("\n 5 : Add a node before a given node");
+        printf("\n 6 : Add a node after a given node");
+        printf("\n 7 : Delete a node from the beginning");
+        printf("\n 8 : Delete a node from the end");
+        printf("\n 9 : Delete a given node");
+        printf("\n 10 : Delete a node after a given node");
+        printf("\n 11 : Delete the entire list");
+        printf("\n 12 : Sort the list");
+        printf("\n 13 : EXIT");
+        printf("\n\n Enter your option : ");
+        scanf("% d", &option);
         switch (option)
         {
         case 1:
             start = create_ll(start);
-            printf(“\n LINKED LIST CREATED”);
+            printf("\n LINKED LIST CREATED");
             break;
         case 2:
             start = display(start);
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
             break;
         case 11:
             start = delete_list(start);
-            printf(“\n LINKED LIST DELETED”);
+            printf("\n LINKED LIST DELETED");
             break;
         case 12:
             start = sort_list(start);
@@ -90,9 +90,9 @@ struct node *create_ll(struct node *start)
 {
     struct node *new_node, *ptr;
     int num;
-    printf(“\n Enter - 1 to end”);
-    printf(“\n Enter the data : “);
-    scanf(“% d”, &num);
+    printf("\n Enter - 1 to end");
+    printf("\n Enter the data : ");
+    scanf("% d", &num);
     while (num != -1)
     {
         new_node = (struct node *)malloc(sizeof(struct node));
@@ -110,8 +110,8 @@ struct node *create_ll(struct node *start)
             ptr->next = new_node;
             new_node->next = NULL;
         }
-        printf(“\n Enter the data : “);
-        scanf(“% d”, &num);
+        printf("\n Enter the data : ");
+        scanf("% d", &num);
     }
     return start;
 }
@@ -121,7 +121,7 @@ struct node *display(struct node *start)
     ptr = start;
     while (ptr != NULL)
     {
-        printf(“\t % d”, ptr->data);
+        printf("\t % d", ptr->data);
         ptr = ptr->next;
     }
     return start;
@@ -130,8 +130,8 @@ struct node *insert_beg(struct node *start)
 {
     struct node *new_node;
     int num;
-    printf(“\n Enter the data : “);
-    scanf(“% d”, &num);
+    printf("\n Enter the data : ");
+    scanf("% d", &num);
     new_node = (struct node *)malloc(sizeof(struct node));
     new_node->data = num;
     new_node->next = start;
@@ -142,8 +142,8 @@ struct node *insert_end(struct node *start)
 {
     struct node *ptr, *new_node;
     int num;
-    printf(“\n Enter the data : “);
-    scanf(“% d”, &num);
+    printf("\n Enter the data : ");
+    scanf("% d", &num);
     new_node = (struct node *)malloc(sizeof(struct node));
     new_node->data = num;
     new_node->next = NULL;
@@ -157,10 +157,10 @@ struct node *insert_before(struct node *start)
 {
     struct node *new_node, *ptr, *preptr;
     int num, val;
-    printf(“\n Enter the data : “);
-    scanf(“% d”, &num);
-    printf(“\n Enter the value before which the data has to be inserted : “);
-    scanf(“% d”, &val);
+    printf("\n Enter the data : ");
+    scanf("% d", &num);
+    printf("\n Enter the value before which the data has to be inserted : ");
+    scanf("% d", &val);
     new_node = (struct node *)malloc(sizeof(struct node));
     new_node->data = num;
     ptr = start;
@@ -178,10 +178,10 @@ struct node *insert_after(struct node *start)
 {
     struct node *new_node, *ptr, *preptr;
     int num, val;
-    printf(“\n Enter the data : “);
-    scanf(“% d”, &num);
-    printf(“\n Enter the value after which the data has to be inserted : “);
-    scanf(“% d”, &val);
+    printf("\n Enter the data : ");
+    scanf("% d", &num);
+    printf("\n Enter the value after which the data has to be inserted : ");
+    scanf("% d", &val);
     new_node = (struct node *)malloc(sizeof(struct node));
     new_node->data = num;
     ptr = start;
@@ -220,8 +220,8 @@ struct node *delete_node(struct node *start)
 {
     struct node *ptr, *preptr;
     int val;
-    printf(“\n Enter the value of the node which has to be deleted : “);
-    scanf(“% d”, &val);
+    printf("\n Enter the value of the node which has to be deleted : ");
+    scanf("%d", &val);
     ptr = start;
     if (ptr->data == val)
     {
@@ -244,8 +244,8 @@ struct node *delete_after(struct node *start)
 {
     struct node *ptr, *preptr;
     int val;
-    printf(“\n Enter the value after which the node has to deleted : “);
-    scanf(“% d”, &val);
+    printf("\n Enter the value after which the node has to deleted : ");
+    scanf("% d", &val);
     ptr = start;
     preptr = ptr;
     while (preptr->data != val)
@@ -265,7 +265,7 @@ struct node *delete_list(struct node *start)
         ptr = start;
         while (ptr != NULL)
         {
-            printf(“\n % d is to be deleted next”, ptr->data);
+            printf("\n % d is to be deleted next", ptr->data);
             start = delete_beg(ptr);
             ptr = start;
         }
